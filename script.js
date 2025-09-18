c692fdd09fdcc46b9126da83624697166a380337
Ashish kumar <151114570+ashish1906@users.noreply.github.com>
2025-09-18 15:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+const form = document.getElementById("myForm");
+const urlElement = document.getElementById("url");
+
+form.addEventListener("submit", function(event){
+	event.preventDefault();
+
+	const name = document.getElementById("name").value;
+      const year = document.getElementById("year").value;
+
+      let baseUrl = "https://localhost:8080/";
+      let query = [];
+
+      if (name) query.push("name=" + encodeURIComponent(name));
+      if (year) query.push("year=" + encodeURIComponent(year));
+
+      if (query.length > 0) {
+        baseUrl += "?" + query.join("&");
+      }
+
+      urlElement.textContent = baseUrl;
+    });
+}
